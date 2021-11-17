f1c89e1a706741456a364fec4feccc9df2f35be8
Kantapon <owliang1234@gmail.com>
2021-11-17 14:53:01 +0700

    Change navbar color

--- frontend/src/components/PageContainer.js
+++ frontend/src/components/PageContainer.js
@@ -1,18 +1,14 @@
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Typography, Row, Col } from 'antd';
 import './PageContainer.css';
 
+const { Title } = Typography;
 const { Header, Content, Footer } = Layout;
 
 export default function PageContainer(props) {
   return ( 
     <Layout className="layout">
-      <Header>
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key={1}>{`nav ${1}`}</Menu.Item>
-          <Menu.Item key={2}>{`nav ${2}`}</Menu.Item>
-          <Menu.Item key={3}>{`nav ${3}`}</Menu.Item>
-        </Menu>
+      <Header style={{ backgroundColor: "#E97D7D" }}>
+        <Title style={{ color: "#FFF", marginTop: '4px'}}>กกต</Title>
       </Header>
       <Content style={{ padding: '0 100px', minHeight: '87.6vh'}}>
         {props.children}
