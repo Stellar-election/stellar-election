6327595f5aa8539db002ac29d5a1e7c74f395594
Kantapon <owliang1234@gmail.com>
2021-11-17 17:10:29 +0700

    Add step on CreateElection page

--- frontend/src/pages/CreateElection/CreateElection.js
+++ frontend/src/pages/CreateElection/CreateElection.js
@@ -1,8 +1,36 @@
-import { CreateElectionDistrictForm } from './CreateElectionDistrictForm'
+import React, { useState } from 'react';
+import { CreateElectionDistrictForm } from './Form/CreateElectionDistrictForm'
+import { CreateElectionCandidateForm } from './Form/CreateElectionCandidateForm'
 import './CreateElection.css'
 
+import { Steps } from 'antd';
+import { UserOutlined, SolutionOutlined, LoadingOutlined, SmileOutlined } from '@ant-design/icons';
+
+const { Step } = Steps;
+
+
+
 export function CreateElection(props) {
+    const [electionInfo, setElectionInfo] = useState({})
+    const [candidateInfo, setCandidateInfo] = useState({})
+
+    const status = {
+        electionInfo: "finish",
+        candidateInfo: "finish",
+        checkInfo: "process",
+        finish: "wait",
+    }
+
     return (
-        <CreateElectionDistrictForm />
+        <>
+            <Steps style={{ marginBottom: "16px"}}>
+                <Step status={status.electionInfo} title="กรอกข้อมูลการเลือกตั้ง" icon={<UserOutlined />} />
+                <Step status={status.candidateInfo} title="กรอกรายชื่อผู้สมัคร" icon={<SolutionOutlined />} />
+                <Step status={status.checkInfo} title="ตรวจสอบข้อมูล" icon={<LoadingOutlined />} />
+                <Step status={status.finish} title="สำเร็จ" icon={<SmileOutlined />} />
+            </Steps>
+            <CreateElectionDistrictForm setElectionInfo={setElectionInfo} />
+            <CreateElectionCandidateForm setCandidateInfo={setCandidateInfo} />
+        </>
     )
 }
---
+++ frontend/src/pages/CreateElection/Form/CreateElectionCandidateForm.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import { Form, Input, Button, Cascader, DatePicker } from 'antd';
+
+const { RangePicker } = DatePicker;
+
+const districtOptions = [
+  {
+    value: 'กรุงเทพ',
+    label: 'กรุงเทพ',
+    children: [
+      {
+        value: 'สะพานสูง',
+        label: 'สะพานสูง',
+      },
+      {
+        value: 'บางกะปิ',
+        label: 'บางกะปิ',
+      },
+      {
+        value: 'พญาไท',
+        label: 'พญาไท',
+      },
+    ],
+  },
+]
+
+export const CreateElectionCandidateForm = (props) => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log(values);
+    props.setElectionInfo(values)
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
+  return (
+    <Form
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 9 }}
+      layout="horizontal"
+      form={form}
+      name="control-hooks"
+      onFinish={onFinish}
+    >
+      <Form.Item
+        label="ชื่อสนามเลือกตั้ง"
+        name="name"
+        rules={[
+          {
+            required: true,
+            message: 'โปรดใส่ชื่อสนามเลือกตั้ง!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      
+      <Form.Item
+        label="พื้นที่"
+        name="district"
+        rules={[
+          {
+            required: true,
+            message: 'โปรดเลือกพื้นที่เลือกตั้ง!',
+          },
+        ]}
+      >
+        <Cascader options={districtOptions} />
+      </Form.Item>
+
+      <Form.Item
+        label="ช่วงเวลาการเลือกตั้ง"
+        name="date"
+        rules={[
+          {
+            type: 'array',
+            required: true,
+            message: 'โปรดเลือกช่วงเวลาการเลือกตั้ง!',
+          },
+        ]}
+      >
+        <RangePicker showTime format="YYYY-MM-DD HH:mm:ss" />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit" style={{ background: "#E97D7D", borderColor: "#E97D7D", marginRight: "8px"}}> 
+          ตกลง
+        </Button>
+        <Button htmlType="button" onClick={onReset}>
+          ล้าง
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
