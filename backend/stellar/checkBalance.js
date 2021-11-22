96777821c9ff8ec865189566ad77a8c7a6fef0df
Kantapon <owliang1234@gmail.com>
2021-11-22 20:52:29 +0700

    add getBalance

--- backend/stellar/checkBalance.js
+++ backend/stellar/checkBalance.js
@@ -8,34 +8,25 @@ const util = require('util')
 // Input : {}
 // Output: { account , secret }
 
-const getBalance = async (account) => (
-    await axios.get('/accounts', {
-        baseURL: 'https://horizon-testnet.stellar.org',
-        params: { addr: account }
-    })
-    
-)
+const getBalance = async (account) => {
+    const sAccount = await server.loadAccount(account);
+    const balance = parseInt(sAccount.balances[0]["balance"])
+    return balance;
+}
 
-
-Router.post('/', function (req, res, next) {
-
+Router.post('/', async (req, res, next) => {
     const account = req.body.account
-    const url = "/accounts/"+ account   
-    const balance = getBalance(url,account)
-    console.log(util.inspect(balance,false,null))
+    const balance = await getBalance(account)
 
     return res.json(
         {
             "status": 200,
             "data": {
                 "address": account,
-                "balance" : balance
-        
+                "result": balance
             }
         }
     )
-
 })
-
 
 module.exports = Router;
